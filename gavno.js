d2f87b2b8ae3a05058e445cfb53e5a94b5ef5893
MaxLakerCoding <mishatronic7045@gmail.com>
2024-10-14 16:44:46 +0300

    вфв

--- gavno.js
+++ gavno.js
@@ -2,40 +2,42 @@ const alphabet = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "�
 const alphabetBox = document.querySelector('.alphabet_box')
 for (let i = 0; i < alphabet.length; i++) {
   const bukva = alphabet[i];
-//   console.log(bukva);
-// const div = document.createElement("div");
-// alphabetBox.appendChild(div);
-// div.classList.add('alphabetLetter');
-// div.textContent = alphabet[i]
-// div.addEventListener('click', () => {
-//   console.log(div);
-// })
-creatingBlock(alphabetBox, alphabet'"alphabetLetter", i)
+
+creatingBlock(alphabetBox, alphabet,"alphabetLetter", "s", i)
 }
 
 const wordBox = document.querySelector('.word')
-const wordArray = ["гусь","бобр","суслик","макака","банан","пицца"];
-let randomindex = Math.floor(Math.random()*4);
+const wordArray = ["гусь","бобр","суслик","макака","банан"];
+const chelParts = document.querySelectorAll(".hidechel")
+console.log(chelParts)
+let randomindex = Math.floor(Math.random()*5);
+let misstakes = 0
 
 let WORD = wordArray[randomindex];
-
 for (let i = 0; i < WORD.length; i++) {
-// const div = document.createElement("div");
-// wordBox.appendChild(div); 
-// div.classList.add('let');
-// div.textContent = WORD[i]
-creatingBlock(wordBox, WORD, "let", i)
+creatingBlock(wordBox, WORD, "let", "hide", i)
 }
+const alpabetButtons = document.querySelectorAll(".alphabetLetter")
+alpabetButtons.forEach((button) => {
+button.addEventListener("click", ()=>{
+    console.log(button.textContent)
+    console.log(WORD.indexOf(button.textContent)) // перевірка наявності букви в слові
+    if (WORD.indexOf(button.textContent)== -1) {
+      console.log("ПОМИЛКА")
+      chelParts[misstakes].classList.remove('hidechel')
+      misstakes++
+      console.log(misstakes)
+    }
+    });
+});
 
-function creatingBlock(parentBlock, slovo, newClass, i){
+function creatingBlock(parentBlock, slovo, newClass, secondClass, i){
   const div = document.createElement("div");
   parentBlock.appendChild(div);
   div.classList.add(newClass);
+  div.classList.add(secondClass);
   div.textContent = slovo[i]
 }
-
-
-
 
 
 
